0e2ca7080f7aef8bbf6a881bc7bb8e7fdcd8645e
Eric Renken <eric@mynoc.com>
2026-07-09 01:05:04 -0400

    Fix HomeComponent test by mocking RemootioAngularService
    - Added mock RemootioAngularService with spy objects
    - Provides gateState\$ observable and isAuthenticated property
    - Fixes test failure: missing required service dependency
    
    HomeComponent requires RemootioAngularService in constructor,
    test was failing because TestBed didn't provide it.

--- angular/src/app/pages/home/home.component.spec.ts
+++ angular/src/app/pages/home/home.component.spec.ts
@@ -1,14 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { RemootioAngularService } from 'remootio-angular';
 
 import { HomeComponent } from './home.component';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let mockRemootioService: jasmine.SpyObj<RemootioAngularService>;
 
   beforeEach(async () => {
+    // Create a mock RemootioAngularService
+    mockRemootioService = jasmine.createSpyObj('RemootioAngularService', [
+      'connect',
+      'closeGate',
+      'openGate'
+    ], {
+      gateState$: of({ isOpen: false, description: 'Closed' }),
+      isAuthenticated: false
+    });
+
     await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ HomeComponent ],
+      providers: [
+        { provide: RemootioAngularService, useValue: mockRemootioService }
+      ]
     })
     .compileComponents();
   });
